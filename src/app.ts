97fcc79fae3731362b8ef7fcacde186a1b8046bb
Jonas Morales <jonasjosuemo2021@outlook.com>
2023-10-12 16:14:54 -0600

    clean achitecture presentation

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,17 @@
-import fs from "fs";
+import { yarg } from "./config/plugins/yargs.plugin";
+import { ServerApp } from "./presentation/serve-app";
 
-let outputMessage: string = '';
-const base: number = 10;
-const headerMessage: string = `
-===========================================================
-            Tabla de multiplicar ${base}
-===========================================================\n
-`;
 
-for (let index = 1; index < 10 ; index++) {
-    outputMessage += `${base} x ${index} = ${base * index}\n`;
+(async () => {
+  await main();
+})();
+
+
+async function main() {
+
+  const { b: base, l: limit, s: showTable, n: fileName, d: fileDestination } = yarg;
+
+
+  ServerApp.run({ base, limit, showTable, fileName, fileDestination });
+
 }
-outputMessage = headerMessage + outputMessage;
-console.log(outputMessage);
-const outPath: string = 'outputs'
-fs.mkdirSync(outPath, {recursive: true})
-fs.writeFileSync(`${outPath}/tabla-${base}.txt`, outputMessage);
-console.log('File created!');
