857911fa50a08504871d6cc44aff5d45cc50d1ef
Jonas Morales <jonasjosuemo2021@outlook.com>
2023-10-16 17:54:47 -0600

    cron service, and check service use case

--- src/presentation/server-app.ts
+++ src/presentation/server-app.ts
@@ -1,5 +1,14 @@
+import { CheckService } from "../domain/use-cases/checks/checks-service";
+import { CronService } from "./cron/cron-service";
+
 export class Server {
     public static start() {
-        console.log('server started');
+        console.log('server started ...');
+        CronService.createJob(
+            '*/5 * * * * *',
+            () => {
+                new CheckService().execute('https://google.com');
+            }
+        )
     }
 }
