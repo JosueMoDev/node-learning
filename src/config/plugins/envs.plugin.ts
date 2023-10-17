4e88051ec484a298916f3240b36b3aeae2234403
Jonas Morales <jonasjosuemo2021@outlook.com>
2023-10-17 11:45:26 -0600

    config env, dotev and env-var packages have been added

---
+++ src/config/plugins/envs.plugin.ts
@@ -0,0 +1,14 @@
+import 'dotenv/config';
+import * as env from 'env-var';
+interface ENVS {
+    PORT: number,
+    MAILER_MAIL: string,
+    MAILER_SECRET_KEY: string,
+    IS_PRODUCTION_MODE: boolean,
+}
+export const envs: ENVS = {
+    PORT: env.get('PORT').required().asPortNumber(),
+    MAILER_MAIL: env.get('MAILER_MAIL').required().asEmailString(),
+    MAILER_SECRET_KEY: env.get('MAILER_SECRET_KEY').required().asString(),
+    IS_PRODUCTION_MODE: env.get('IS_PRODUCTION_MODE').required().asBool(),
+}
