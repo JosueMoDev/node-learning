45bd855af1d9040027124ab16f257c95c5e1a2f7
Jonas Morales <jonasjosuemo2021@outlook.com>
2023-10-17 11:43:39 -0600

    log entity created

--- src/domain/entities/log.entity.ts
+++ src/domain/entities/log.entity.ts
@@ -17,4 +17,18 @@ export class LogEntity {
         this.createdAt = new Date();
     }
 
+    static fronJson = (json: string): LogEntity => { 
+        
+        const { message, level, createdAt } = JSON.parse(json);
+        if (!message) throw new Error(`You Must Pride a message`);
+        if (!level) throw new Error(`You Must Pride a severity level`);
+
+        const log = new LogEntity(message, level);
+        log.createdAt = new Date(createdAt);
+
+        return log;
+        
+    }
+
+
 }
