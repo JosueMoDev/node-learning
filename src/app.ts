993624317445c41ca88ed9f0d22ed56168f846dd
Jonas Morales <jonasjosuemo2021@outlook.com>
2023-10-19 10:06:34 -0600

    postgres data source implemented and algo multiple repository use case

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import { PrismaClient } from "@prisma/client";
 import { envs } from "./config/plugins/envs.plugin";
 import { MongoDatabase } from "./data/mongo-db/init";
 import { LogModel } from "./data/mongo-db/models/log.model";
@@ -16,13 +17,23 @@ async function main() {
         dbName: envs.MONGO_DB_NAME
     });
 
-    const newLog = await LogModel.create({
-        message: 'Test from Mongo',
-        origin: 'app.ts',
-        level: LogSeverityLevel.low
-    });
+    // const newLog = await LogModel.create({
+    //     message: 'Test from Mongo',
+    //     origin: 'app.ts',
+    //     level: LogSeverityLevel.low
+    // });
+    // await newLog.save();
 
-    await newLog.save();
+    // const prisma = new PrismaClient();
+    // const newLog = await prisma.logModel.create({
+    //     data: {
+    //         level: 'HIGH',
+    //         message: 'Test usign prisma and postgres',
+    //         origin: 'App.ts'
+    //     }
+    // });
+
+    // console.log(newLog);
 
     Server.start();
 }
--- src/presentation/server-app.ts
+++ src/presentation/server-app.ts
@@ -1,15 +1,18 @@
 import { FileSystemDatasource } from "../Infraestructure/datasources/file-system.datasource";
 import { MongoLogDatasource } from "../Infraestructure/datasources/mongo-log.datasource";
+import { PostgresLogDataSource } from "../Infraestructure/datasources/postgres-log.datasource";
 import { LogRepositoryImpl } from "../Infraestructure/repositories/log.repository.impl";
 import { envs } from "../config/plugins/envs.plugin";
 import { LogSeverityLevel } from "../domain/entities/log.entity";
 import { CheckService } from "../domain/use-cases/checks/checks-service";
+import { CheckServiceMultiple } from "../domain/use-cases/checks/checks-service-multiple";
 import { SendEmailLogs } from "../domain/use-cases/email/send-email-logs";
 import { CronService } from "./cron/cron-service";
 import { EmailService } from "./email/email-service";
 
 const fileSystemLogRepository = new LogRepositoryImpl(new FileSystemDatasource());
 const mongoLogRepository = new LogRepositoryImpl(new MongoLogDatasource());
+const postgresLogRepository = new LogRepositoryImpl(new PostgresLogDataSource());
 
 const emailService = new EmailService();
 export class Server {
@@ -34,18 +37,27 @@ export class Server {
         // })
 
         // emailService.sendEmailWithFileSystemLogs('josuemodev@gmail.com');
-
-        // const logs = await fileSystemLogRepository.getLog(LogSeverityLevel.medium);
-        // console.log(logs)
+      
         // CronService.createJob(
         //     '*/5 * * * * *',
         //     () => {
         //         new CheckService(
-        //             mongoLogRepository,
+        //             postgresLogRepository,
         //             () => console.log('service up'),
         //             () => console.log('Error on service'),
         //         ).execute("http://localhost:3000");
         //     }
-        // )
+        // );
+
+        CronService.createJob(
+            '*/5 * * * * *',
+            () => {
+                new CheckServiceMultiple(
+                    [postgresLogRepository, mongoLogRepository, fileSystemLogRepository],
+                    () => console.log('service up'),
+                    () => console.log('Error on service'),
+                ).execute("http://localhost:3000");
+            }
+        )
     }
 }
