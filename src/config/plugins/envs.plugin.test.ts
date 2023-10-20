248a7f395d9d7f52cce04341be70576f00e30c5f
Jonas Morales <josuemodev@gmail.com>
2023-10-19 18:00:22 -0600

    envs testing

--- src/config/plugins/envs.plugin.test.ts
+++ src/config/plugins/envs.plugin.test.ts
@@ -3,7 +3,33 @@ import { envs } from "./envs.plugin";
 describe('envs.plugin.ts', () => {
 
     test('Should return envs variables', () => {
-        console.log(envs)
-    })
+        expect(envs).toEqual({
+          PORT: 3000,
+          MAILER_MAIL: "jonasjosuemoralese@gmail.com",
+          MAILER_SECRET_KEY: "qwljelkjrlkjler",
+          IS_PRODUCTION_MODE: true,
+          MAILER_SERVICE: "gmail",
+          MONGO_URL: "mongodb://test:123456789@localhost:27017",
+          MONGO_DB_NAME: "nocdb-test",
+          MONGO_USER_NAME: "test",
+          MONGO_PASS: "123456789",
+          POSTGRES_URL:
+            "postgresql://postgres:mysecretpasswordTest@localhost:5432/nocdb-test",
+          POSTGRES_USER: "postgres",
+          POSTGRES_PASSWORD: "mysecretpasswordTest",
+          POSTGRES_DB: "nocdb-test",
+        });
+    });
+
+    test('Should return error if not found env', async () => {
+        jest.resetModules();
+        process.env.PORT = 'ABC'
+        try {
+            await import('./envs.plugin');
+            expect(true).toBeFalsy;
+        } catch (error) {
+            expect(`${error}`).toContain('"PORT" should be a valid integer');
+        }
+    });
 
 });
