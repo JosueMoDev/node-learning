6ca0845d854d249bebfa774edd7f0abcf8c6c88d
Jonas Morales <jonasjosuemo2021@outlook.com>
2023-11-11 11:58:06 -0600

    mongo config, seeder, models

---
+++ src/data/index.ts
@@ -0,0 +1,6 @@
+export * from './mongo';
+export * from './mongo/models/category.model';
+export * from './mongo/models/product.model';
+export * from './mongo/models/user.model';
+export * from './seed/data';
+export * from './seed/seed';
